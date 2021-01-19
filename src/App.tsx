66ae589168e5ea479cca6ae0bd8b4e2e2d66f710
tomi-mercado <tmercadoslp@gmail.com>
2021-01-19 08:26:54 -0300

    (App): set interval and re-render city

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,8 @@ const coordinates: Array<Coordinate> = [
     longitude: -73.935242,
   },
   {
-    latitude: -37.58,
-    longitude: -57.35,
+    latitude: 37.58,
+    longitude: -47.35,
   },
 ];
 
@@ -34,6 +34,7 @@ function App() {
   //TODO: type this
   const [weatherCities, setWeatherCities] = useState<Array<any>>([]);
   const [currentCityIndex, setCurrentCityIndex] = useState(0);
+  const [errorLoading, setErrorLoading] = useState(false);
 
   const getByLattLong = async (lat, lon) => axios.get(`${apiRoute}/location/search/?lattlong=${lat},${lon}`, {
     headers: {
@@ -45,6 +46,7 @@ function App() {
       'Access-Control-Allow-Origin': '*'
     }
   });
+  const haveWeatherCitiesLoaded = weatherCities.length > 0;
 
   useEffect(() => {
     Promise.all(coordinates.map(async ({ latitude, longitude }) => {
@@ -56,20 +58,33 @@ function App() {
       const currentWeatherCity = weatherCity.consolidated_weather[0];
       return {
         name: cityFound.title,
-        max: currentWeatherCity.max_temp,
-        min: currentWeatherCity.min_temp,
-        humidity: currentWeatherCity.humidity,
+        max: Math.round(currentWeatherCity.max_temp),
+        min: Math.round(currentWeatherCity.min_temp),
+        humidity: Math.round(currentWeatherCity.humidity),
         icon: `https://www.metaweather.com/static/img/weather/${currentWeatherCity.weather_state_abbr}.svg`
       };
     }))
-    .then((result) => setWeatherCities(result));
+    .then((result) => setWeatherCities(result))
+    .catch((_error) => setErrorLoading(true))
   }, []);
 
-  const currentCity = weatherCities[currentCityIndex]
+  useEffect(() => {
+    if (haveWeatherCitiesLoaded) {
+      const interval = setInterval(() => {
+        setCurrentCityIndex(currentIndex => {
+          console.log("Current index", currentIndex)
+          console.log("Length", weatherCities.length)
+          return currentIndex !== weatherCities.length - 1 ? currentCityIndex + 1 : 0
+        });
+      }, 10000);
+      return () => clearInterval(interval);
+    }
+  }, [currentCityIndex, haveWeatherCitiesLoaded, weatherCities.length]);
+
+  const currentCity = weatherCities[currentCityIndex];
   
-  return (
+  if (haveWeatherCitiesLoaded) return (
     <div>
-      {weatherCities.length > 0 && (
         <CityCard
           name={currentCity.name}
           max={currentCity.max}
@@ -77,9 +92,12 @@ function App() {
           humidity={currentCity.humidity}
           icon={currentCity.icon}
         />
-      )}
     </div>
   );
+
+  if (errorLoading) return <h1>Error loading cities</h1>
+
+  return <h1>Cities loading...</h1>
 }
 
 export default App;
